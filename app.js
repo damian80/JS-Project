792b39b1ce5cf813bb09e93f99054ee68e4f28f6
damian80 <daneek80@gmail.com>
2020-11-12 22:12:03 +0000

    final lines of JS code

--- app.js
+++ app.js
@@ -3,23 +3,58 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 
-// GET QUOTE FROM API
-async function getQuote() {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-    try {
-        const response = await fetch(proxyUrl + apiUrl);
-        const data = await response.json();
-        authorText.innerText = data.quoteAuthor;
-        quoteText.innnerText = data.quoteText;
-    } catch (error) {
-        getQuote();
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
     }
 }
 
 
+async function getQuoteFromAPI() {
+    showLoadingSpinner();
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        // if author is blamk, add "unknown"
+        if (data.authorText === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // reduce font size for a long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innnerText = data.quoteText;
+        // STOP loader, SHOW QUOTE
+        removeLoadingSpinner();
+    } catch (error) {
+        getQuoteFromAPI();
+    }
+}
+// tweet quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+// event listener
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 // FOR LOAD
 getQuote();
